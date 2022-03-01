ff506f547f3f3bd27779aeb81fa3350c731ecab9
DaylightNebula <35437205+DaylightNebula@users.noreply.github.com>
2022-03-01 15:16:38 -0700

    got the server working :)

--- testnode/start.bat
+++ testnode/start.bat
@@ -1,2 +1,2 @@
-java -jar -DIReallyKnowWhatIAmDoingISwear paper-1.18.1-215.jar
+"C:\Program Files\Eclipse Adoptium\jdk-17.0.1.12-hotspot\bin\java.exe" -jar -DIReallyKnowWhatIAmDoingISwear paper-1.18.1-215.jar nogui
 PAUSE
